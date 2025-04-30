f74683c48e7380012e7e5f2ab3fd9e0abf6f47d0
danieldo7002@gmail.com <danieldo7002@gmail.com>
2025-04-30 16:37:21 -0300

    atualização

--- tst1.py
+++ tst1.py
@@ -1,1 +1,11 @@
-print('hello World')
+print('Calcular área do círculo')
+
+raio = 5.13
+
+area = 3.14 * raio ** 2
+
+print("Área do circulo:", area)
+
+print('A área do círculo de raio', raio, 'é', area)
+
+print(f'A área do círculo de raio {raio} é {area:.4f}')
---
+++ tst2.py
@@ -0,0 +1,10 @@
+print('cálculo da média aritmética de 4 notas')
+
+nota1 = 6.5
+nota2 = 7.0
+nota3 = 4.9
+nota4 = 6.3
+
+media = ((nota1 + nota2 + nota3 + nota4) / 2)
+
+print(f'A média desse aluno é: {media:.1f}')
