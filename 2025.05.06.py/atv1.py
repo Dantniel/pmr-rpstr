025f3ea842c405ea657d19093470556cb09ff324
danieldo7002@gmail.com <danieldo7002@gmail.com>
2025-05-06 17:41:45 -0300

    atl.2

--- 2025.05.06.py/atv1.py
+++ 2025.05.06.py/atv1.py
@@ -1,12 +1,16 @@
 # 1. Em um movimento retílineo uniformemente variado (MRUV), a fórmula para calcular a distância percorrida é dada po:
 # Delta S = Velocidade inicial * tempo + Aceleração * tempo ao quadrado sobre 2 
 
-velo_ini = float(input('Informe a velocidade inicial: '))
+import sys
+
+velo_ini = int(input('Informe a velocidade inicial: '))
+if velo_ini <= 0 :
+    sys.exit('Informe uma velocidade positiva')
 
 tempo = float(input('informe o tempo: '))
 
-acel = float(input('Informe a aceleração: '))
+acel = int(input('Informe a aceleração: '))
 
 delta_s = velo_ini * tempo + (acel * (tempo * tempo)) / 2
 
-print(f'A velocidade do motorista é {delta_s}')
+print(f'A velocidade do motorista é {delta_s}') 
