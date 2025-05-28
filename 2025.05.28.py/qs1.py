a78b07c005e4a10281441180456788689d6509c4
danieldo7002@gmail.com <danieldo7002@gmail.com>
2025-05-28 17:47:23 -0300

    t0

---
+++ 2025.05.28.py/qs1.py
@@ -0,0 +1,33 @@
+# Programa que solicite ao usúario números inteiros
+# aleatoriamente até que seja informado o valor 0.
+# Quando for digitado o valor 0, o programa deverá informar:
+# A) Quantos números inteiro foram digitado;
+# B) A soma dos números inteiros positivos;
+# C) A média dos números inteiros positivos;
+# O valor 0 digitado não deve ser considerado em nenhum dos iten acima.
+from logging import exception
+import sys
+from turtle import numinput
+
+valor = None
+somaposi = 0
+qtval = 0
+qtvalposi = 0
+
+while valor != 0:
+    try:
+        valor = int(input('Informe um valor inteiro: '))
+    except ValueError:
+        print('Erro: valor inteiro inválido')
+    except Exception as e:
+        print(f"Erro: {e}")
+    else:
+        if valor > 0:
+            somaposi += valor
+            qtvalposi += 1
+        
+    qtval += 1
+
+print(f'Quantos números inteiro foram digitados: {qtval}')
+print(f'Soma dos números inteiros positivos: {somaposi}')
+print(f'Média dos números inteiros positivos: {somaposi/qtvalposi}')
