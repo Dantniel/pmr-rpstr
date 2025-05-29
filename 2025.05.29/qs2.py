92341400204f6c4cdef1a2959cf08884375a2e1d
danieldo7002@gmail.com <danieldo7002@gmail.com>
2025-05-29 17:52:14 -0300

    t0

--- 2025.05.29/qs2.py
+++ 2025.05.29/qs2.py
@@ -1,3 +1,23 @@
 '''
 
 '''
+import sys
+
+try:
+    valor = int(input('Digite um valor inteiro positivo: '))
+except ValueError:
+    sys.exit('Valor não correspondente.')
+except Exception as e:
+    sys.exit('Valor não correspondente.')
+else:
+    if valor < 0:
+        print('Não é possivel um fatorial negativo.')
+    elif valor < 2 :
+        print(f'{valor}! = 1')
+    else:
+        fatorial = valor
+        avx = valor
+        while avx <= 2:
+            avx -= 1
+            fatorial *= avx
+print(f'{valor}! = {fatorial}')
